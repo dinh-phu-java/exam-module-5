f481ec950491828844083b7ba69fe22b92697eea
Dinh Phu <ndp17hp@gmail.com>
2020-08-24 09:30:48 +0700

    final ver1

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BookAddComponent } from './book-add/book-add.component';
+import { BookDeleteComponent } from './book-delete/book-delete.component';
+import { BookEditComponent } from './book-edit/book-edit.component';
+import { BookDetailComponent } from './book-detail/book-detail.component';
+import { BookListComponent } from './book-list/book-list.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BookAddComponent,
+    BookDeleteComponent,
+    BookEditComponent,
+    BookDetailComponent,
+    BookListComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
